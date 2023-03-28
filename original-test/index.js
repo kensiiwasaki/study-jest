9735ddfab07210aeddac6668bb75b0f737c5a312
kensiiwasaki <kensiiwasaki@gmail.com>
2023-03-28 12:08:36 +0900

    modify: コメント削除

--- original-test/index.js
+++ original-test/index.js
@@ -2,13 +2,6 @@ function sum(a, b) {
   return a + b
 }
 
-// const result = sum(1, 2)
-// const expected = 4
-
-// if (result !== expected) {
-//   throw new Error(`${result} is not equal to ${expected}`)
-// }
-
 expect(sum(1, 2)).toBe(4)
 
 function expect(result) {
