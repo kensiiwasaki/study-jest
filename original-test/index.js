bbca5d59f3c04d2e0b48410e8fbd7fd788d6f6dd
kensiiwasaki <kensiiwasaki@gmail.com>
2023-03-28 12:19:29 +0900

    成功失敗をわかりやすくする

--- original-test/index.js
+++ original-test/index.js
@@ -3,7 +3,7 @@ function sum(a, b) {
 }
 
 test('adds 1 + 2 to equal 3', function () {
-  expect(sum(1, 2)).toBe(4)
+  expect(sum(1, 2)).toBe(3)
 })
 
 test('adds 3 + 4 to equal 7', function () {
@@ -13,8 +13,10 @@ test('adds 3 + 4 to equal 7', function () {
 function test(title, callback) {
   try {
     callback()
+    console.log(`✅ ${title}`)
   } catch (error) {
     console.error(error)
+    console.error(`❌ ${title}`)
   }
 }
 
