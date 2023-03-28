ed423643a490d2b18179ece248d135ab645e8779
kensiiwasaki <kensiiwasaki@gmail.com>
2023-03-28 12:24:52 +0900

    コメント追加

--- original-test/index.js
+++ original-test/index.js
@@ -1,15 +1,17 @@
+// テストする関数
 function sum(a, b) {
   return a + b
 }
 
+// テストケース
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
 })
-
 test('adds 3 + 4 to equal 7', () => {
   expect(sum(3, 4)).toBe(8)
 })
 
+// テスト処理の実装
 async function test(title, callback) {
   try {
     await callback()
