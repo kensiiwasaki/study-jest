c8e2b2a7ef858410753b61c7989c6f83065dc7e0
kensiiwasaki <kensiiwasaki@gmail.com>
2023-03-28 12:17:24 +0900

    エラーが出た時に処理が止まらないようにtry-catchを使用

--- original-test/index.js
+++ original-test/index.js
@@ -2,7 +2,21 @@ function sum(a, b) {
   return a + b
 }
 
-expect(sum(1, 2)).toBe(4)
+test('adds 1 + 2 to equal 3', function () {
+  expect(sum(1, 2)).toBe(4)
+})
+
+test('adds 3 + 4 to equal 7', function () {
+  expect(sum(3, 4)).toBe(8)
+})
+
+function test(title, callback) {
+  try {
+    callback()
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 function expect(result) {
   return {
