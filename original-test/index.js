04850e8f99090e1d1b88da9fec6327ac284c0509
kensiiwasaki <kensiiwasaki@gmail.com>
2023-03-28 12:08:13 +0900

    modify: コードの抽象化

--- original-test/index.js
+++ original-test/index.js
@@ -2,9 +2,21 @@ function sum(a, b) {
   return a + b
 }
 
-const result = sum(1, 2)
-const expected = 4
+// const result = sum(1, 2)
+// const expected = 4
 
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+// if (result !== expected) {
+//   throw new Error(`${result} is not equal to ${expected}`)
+// }
+
+expect(sum(1, 2)).toBe(4)
+
+function expect(result) {
+  return {
+    toBe(expected) {
+      if (result !== expected) {
+        throw new Error(`${result} is not equal to ${expected}`)
+      }
+    },
+  }
 }
