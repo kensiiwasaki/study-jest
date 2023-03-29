904fa3efba6de2d751d19fea484ec49a88474a80
kensiiwasaki <kensiiwasaki@gmail.com>
2023-03-29 11:48:01 +0900

    テストケース増やしてみた

--- utils/sum.test.ts
+++ utils/sum.test.ts
@@ -3,3 +3,19 @@ const sum = require('./sum')
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
 })
+
+test('adds 3 + 4 to equal 7', () => {
+  expect(sum(3, 4)).toBe(7)
+})
+
+test('adds 6 + 4 to equal 10', () => {
+  expect(sum(6, 4)).toBe(10)
+})
+
+test('adds 200 + 200 to equal 400', () => {
+  expect(sum(200, 200)).toBe(400)
+})
+
+test('adds 20 + -40 to equal -20', () => {
+  expect(sum(20, -40)).toBe(-20)
+})
