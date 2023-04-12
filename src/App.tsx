05f1922eeb83b7c0914dfdd036379814d6803602
kensiiwasaki <kensiiwasaki@gmail.com>
2023-04-12 11:37:50 +0900

    modify: パスの変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
-import { SimpleButton } from './SimpleButton'
+import { SimpleButton } from './components/SimpleButton'
 
 function App() {
   return (
