719ffc6c4d73cf111e113d740abb037ff1c3d0be
kensiiwasaki <kensiiwasaki@gmail.com>
2023-04-12 11:38:19 +0900

    modify: パスの変更によるファイルの削除

--- src/SimpleButton.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, useState } from 'react'
-
-export const SimpleButton: FC = () => {
-  const [state, setState] = useState(false)
-  const handleClick = () => {
-    setState((prevState) => !prevState)
-  }
-  return <button onClick={handleClick}>{state ? 'ON' : 'OFF'}</button>
-}
