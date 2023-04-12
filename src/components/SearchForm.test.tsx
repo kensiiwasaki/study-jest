7eb47ecdaf70231748c7418cdb171a40561b9d5d
kensiiwasaki <kensiiwasaki@gmail.com>
2023-04-12 14:59:28 +0900

    modify: 検索ボタンを押した時の挙動のテストを追加

--- src/components/SearchForm.test.tsx
+++ src/components/SearchForm.test.tsx
@@ -7,8 +7,13 @@ describe('input form onchange event', () => {
   it('input test', () => {
     render(<SearchForm />)
     const inputValue = screen.getByRole('textbox') as HTMLInputElement
+    const searchBtn = screen.getByRole('button')
+    // インプットの中にtestと打った時にtestと入力されているか
     userEvent.type(inputValue, 'test')
-
     expect(inputValue.value).toBe('test')
+
+    // 検索ボタンを押した際にインプットの中身が空になるかテスト
+    userEvent.click(searchBtn)
+    expect(inputValue.value).toBe('')
   })
 })
